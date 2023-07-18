4544ecd47df1f59e0c8f80b82d93ec74b4a06532
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2023-07-18 15:57:04 -0700

    Added check for session on image upload. Simplified form data.

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,13 +1,21 @@
 "use server";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 const url = "https://api.cloudinary.com/v1_1/dqzg3fumi/image/upload";
 
 export async function addItem(formData) {
-  const newImg = formData.get("newImage");
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  formData.append("file", newImg);
+  if (!session) {
+    console.error("No Session, Must be logged in to add a photo");
+    return;
+  }
+
   formData.append("cloud_name", "dqzg3fumi");
   formData.append("upload_preset", "test_preset");
-
   const configObj = {
     method: "post",
     body: formData,
@@ -15,7 +23,9 @@ export async function addItem(formData) {
 
   const res = await fetch(url, configObj);
   const data = await res.json();
+
   console.log("Response: ", data);
+
   //   If good response, add url to db
 
   // otherwise handle error
--- src/app/newImgForm.js
+++ src/app/newImgForm.js
@@ -4,13 +4,8 @@ import { addItem } from "./actions";
 export default function newImgForm() {
   return (
     <form style={{ padding: "50px" }} action={addItem}>
-      <label htmlFor="new-image">New Post</label>
-      <input
-        type="file"
-        id="new-image"
-        name="newImage"
-        accept="image/png, image/jpeg"
-      />
+      <label htmlFor="file">New Post</label>
+      <input type="file" id="file" name="file" accept="image/png, image/jpeg" />
       <button type="submit">YEET</button>
     </form>
   );
