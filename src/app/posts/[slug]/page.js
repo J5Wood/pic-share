0d93acf7569e9d991637c658586cb4d863face1a
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2023-08-02 21:37:14 -0700

    Added single post page logic

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,6 +1,32 @@
-import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Image from "next/image";
 
-export default function Page({ params }) {
-  const slug = params.slug;
-  return <div>Post page: {slug}</div>;
+export default async function Page({ params: { slug } }) {
+  console.log(slug);
+
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data } = await supabase
+    .from("posts")
+    .select()
+    .match({ id: slug })
+    .single();
+
+  return (
+    <div className="posts-container">
+      <div className="post-card" href={`posts/${data.id.toString()}`}>
+        <Image
+          src={data.url}
+          width={250}
+          height={250}
+          alt="WHERE'S MY ALT TEXT?!?!"
+        />
+        <div>
+          <h4>@{data.username}: </h4>
+          <p>{data.content}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
