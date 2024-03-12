7b08e07f3535cc665f777822659c3193cfa6ed3e
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2024-03-12 10:19:25 -0700

    Finished heart component tests

--- src/__tests__/heart.test.jsx
+++ src/__tests__/heart.test.jsx
@@ -1,14 +1,21 @@
 import { expect, test } from "vitest";
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Heart from "../app/components/heart";
 
-test("Heart displays properly", async () => {
-  const session = true;
-  const postLiked = true;
-  const postId = 12;
+test("Liked heart displays properly", async () => {
+  render(<Heart session={true} postLiked={true} postId={12} />);
+  const heart = await screen.findByRole("heart");
+  expect(heart.innerHTML).toBe("♥︎");
+});
 
-  render(<Heart session={session} postLiked={postLiked} postId={postId} />);
-  console.log("HERE: ", await screen.findByRole("heart"));
-  // const main = findByRole("main");
-  // expect("main").toBeDefined();
+test("Unliked heart displays properly", async () => {
+  render(<Heart session={true} postLiked={false} postId={12} />);
+  const heart = await screen.findByRole("heart");
+  expect(heart.innerHTML).toBe("♡");
+});
+
+test("Heart not displayed when there is no session", async () => {
+  render(<Heart session={false} postLiked={false} postId={12} />);
+  const heart = await screen.queryByRole("heart");
+  expect(heart).toBeNull();
 });
