acfefa6154b2c56ce3caa66fb6163b2199d2c9c8
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2024-03-23 00:04:43 -0700

    Updated next. Added basic testing for a post

---
+++ src/__tests__/post.test.jsx
@@ -0,0 +1,31 @@
+import Post from "../app/components/Post";
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+const postData = {
+  url: "https://jeremywood.tech/_astro/bust.ed0c9b3a.png",
+  liked: true,
+  session: true,
+  id: 12,
+  inserted_at: "2023-07-31 21:23:14.725161+00",
+  username: "Billiam",
+  content: "Magna fringilla urna porttitor rhoncus dolor purus.",
+};
+
+test("A post should display an image", async () => {
+  render(<Post post={postData} />);
+  const postImage = await screen.getByRole("post-image");
+  expect(postImage).toBeDefined();
+  expect(postImage.src).toBeDefined();
+});
+
+test("A post should list the poster and content", async () => {
+  render(<Post post={postData} />);
+  const postContent = await screen.getByRole("post-content");
+  console.log("POST", postContent.children[0].innerHTML);
+  expect(postContent).toBeDefined();
+  expect(postContent.children[0].innerHTML).toMatch("@Billiam");
+  expect(postContent.children[1].innerHTML).toMatch(
+    "Magna fringilla urna porttitor rhoncus dolor purus."
+  );
+});
